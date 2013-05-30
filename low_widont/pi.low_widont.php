ca764b1f45519693c5621af71f7ece45bfc8f6cc
Lodewijk Schutte <lodewijk@gmail.com>
2013-05-30 14:16:21 +0200

    v210

--- low_widont/pi.low_widont.php
+++ low_widont/pi.low_widont.php
@@ -2,114 +2,76 @@
 
 $plugin_info = array(
 	'pi_name'			=> "Low Widon&rsquo;t",
-	'pi_version'		=> '2.0',
+	'pi_version'		=> '2.1.0',
 	'pi_author'			=> 'Lodewijk Schutte ~ Low',
-	'pi_author_url'		=> 'http://loweblog.com/freelance/article/ee-widont-plugin/',
-	'pi_description'	=> 'Eliminates widows in your post titles by inserting a non-breaking space between the last two words of, for example, a title.',
-	'pi_usage'			=> Low_widont::usage()
+	'pi_author_url'		=> 'http://gotolow.com/addons/low-widont',
+	'pi_description'	=> 'Eliminates typographical widows.',
+	'pi_usage'			=> '{exp:low_widont}Your text here{/exp:low_widont}'
 );
 
 /**
-* Low Widon't Plugin class (based on the WordPress Widon't plugin by Shaun Inman)
-*
-* @package			low-widont-ee2_addon
-* @version			2.0
-* @author			Lodewijk Schutte ~ Low <low@loweblog.com>
-* @link				http://loweblog.com/freelance/article/ee-widont-plugin/
-* @license			http://creativecommons.org/licenses/by-sa/3.0/
-*/
+ * < EE 2.6.0 backward compat
+ */
+if ( ! function_exists('ee'))
+{
+	function ee()
+	{
+		static $EE;
+		if ( ! $EE) $EE = get_instance();
+		return $EE;
+	}
+}
+
+/**
+ * Low Widon't Plugin class (based on the WordPress Widon't plugin by Shaun Inman)
+ *
+ * @package        low_widont
+ * @author         Lodewijk Schutte <hi@gotolow.com>
+ * @link           http://gotolow.com/addons/low-widont
+ * @license        http://creativecommons.org/licenses/by-sa/3.0/
+ */
 class Low_widont {
 
-	/**
-	* Plugin return data
-	*
-	* @var	string
-	*/
-	var $return_data;
-
+	// --------------------------------------------------------------------
+	// PROPERTIES
 	// --------------------------------------------------------------------
 
 	/**
-	* PHP4 Constructor
-	*
-	* @see	__construct()
-	*/
-	function Low_widont($str = '')
-	{
-		$this->__construct($str);
-	}
+	 * Plugin return data
+	 *
+	 * @var        string
+	 */
+	public $return_data;
 
+	// --------------------------------------------------------------------
+	// METHODS
 	// --------------------------------------------------------------------
 
 	/**
-	* PHP 5 Constructor
-	*
-	* @param	string	$str	String to apply the widont algorithm to
-	* @return	string
-	*/
-	function __construct($str = '')
+	 * Constructor
+	 *
+	 * @param      string
+	 * @return     string
+	 */
+	public function __construct($str = '')
 	{
-		/** -------------------------------------
-		/**  Get global instance
-		/** -------------------------------------*/
+		// -------------------------------------------
+		// Get string to work with
+		// -------------------------------------------
 
-		$this->EE =& get_instance();
+		$this->return_data = ($str) ? $str : ee()->TMPL->tagdata;
 
-		/** -------------------------------------
-		/**  Get string to work with
-		/** -------------------------------------*/
-		
-		$this->return_data = ($str) ? $str : $this->EE->TMPL->tagdata;
+		// -------------------------------------------
+		// Apply widon't algorithm
+		// -------------------------------------------
 
-		/** -------------------------------------
-		/**  Apply widon't algorithm
-		/** -------------------------------------*/
-
-		$this->_marry();
-		
-		/** -------------------------------------
-		/**  Return new spiffy widonty string
-		/** -------------------------------------*/
-		
-		return $this->return_data;
-	}
-
-	// --------------------------------------------------------------------
-
-	/**
-	* Marry - applies widon't algorithm to string
-	*
-	* @access	private
-	*/
-	function _marry()
-	{
 		$this->return_data = preg_replace('/\s+(\S+)$/', '&#160;$1', rtrim($this->return_data));
-	}
 
-	// --------------------------------------------------------------------
+		// -------------------------------------------
+		// Return for good measure
+		// -------------------------------------------
 
-	/**
-	* Plugin Usage
-	*
-	* @return	string
-	*/
-	function usage()
-	{
-		ob_start(); 
-		?>
-			What is a widow? In typesetting, a widow is a single word on a line by itself at the end
-			of a paragraph and is considered bad style.
-
-			To use this plugin, wrap anything you want to be processed by it between these tag pairs.
-			The title of a weblog entry works best:
-
-			{exp:low_widont}{title}{/exp:low_widont}
-		<?php
-		$buffer = ob_get_contents();
-	
-		ob_end_clean(); 
-
-		return $buffer;
+		return $this->return_data;
 	}
 
 	// --------------------------------------------------------------------
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Low Widon't for ExpressionEngine 2
+
+See [gotolow.com](http://gotolow.com/addons/low-widont) for more info.
