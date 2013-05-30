b96577af5c2aadc268ae064a96184f5d2ddcf625
Lodewijk Schutte <lodewijk@gmail.com>
2013-05-30 14:19:37 +0200

    tabs vs spaces

--- low_widont/pi.low_widont.php
+++ low_widont/pi.low_widont.php
@@ -1,12 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $plugin_info = array(
-	'pi_name'			=> "Low Widon&rsquo;t",
-	'pi_version'		=> '2.1.0',
-	'pi_author'			=> 'Lodewijk Schutte ~ Low',
-	'pi_author_url'		=> 'http://gotolow.com/addons/low-widont',
-	'pi_description'	=> 'Eliminates typographical widows.',
-	'pi_usage'			=> '{exp:low_widont}Your text here{/exp:low_widont}'
+	'pi_name'        => "Low Widon&rsquo;t",
+	'pi_version'     => '2.1.0',
+	'pi_author'      => 'Lodewijk Schutte ~ Low',
+	'pi_author_url'  => 'http://gotolow.com/addons/low-widont',
+	'pi_description' => 'Eliminates typographical widows.',
+	'pi_usage'       => '{exp:low_widont}Your text here{/exp:low_widont}'
 );
 
 /**
